22d8f29f7cef0a29497c4a412793416601b6918d
NurKeinNeid <nurkeinneid@derpfest.org>
2026-09-10 19:19:16 +0200

    DerpFestCustomizations: Hide Pulse prefs the selected style cannot use
    Rounded bars only apply to solid and waveform, and Retro VU ignores color.
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/derpfest/customizations/fragment/PulseSettings.java
+++ src/org/derpfest/customizations/fragment/PulseSettings.java
@@ -7,17 +7,67 @@ package org.derpfest.customizations.fragment;
 
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class PulseSettings extends SettingsPreferenceFragment {
+public class PulseSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_PULSE_RENDERER = "pulse_renderer";
+    private static final String KEY_PULSE_ROUNDED_BARS = "pulse_rounded_bars";
+    private static final String KEY_PULSE_COLOR = "pulse_color";
+
+    private ListPreference mPulseRenderer;
+    private Preference mPulseRoundedBars;
+    private Preference mPulseColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pulse_settings);
+
+        mPulseRenderer = findPreference(KEY_PULSE_RENDERER);
+        mPulseRoundedBars = findPreference(KEY_PULSE_ROUNDED_BARS);
+        mPulseColor = findPreference(KEY_PULSE_COLOR);
+
+        if (mPulseRenderer != null) {
+            mPulseRenderer.setOnPreferenceChangeListener(this);
+            updatePreferenceVisibility(mPulseRenderer.getValue());
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPulseRenderer) {
+            updatePreferenceVisibility((String) newValue);
+        }
+        return true;
+    }
+
+    /**
+     * Rounding is only applied by SolidLine and Waveform in SystemUI.
+     * Retro VU uses hardcoded segment colors and ignores pulse_color.
+     */
+    private void updatePreferenceVisibility(String rendererValue) {
+        if (rendererValue == null) {
+            return;
+        }
+
+        boolean supportsRounding = "solid".equals(rendererValue)
+                || "waveform".equals(rendererValue);
+        boolean supportsColoring = !"retro".equals(rendererValue);
+
+        if (mPulseRoundedBars != null) {
+            mPulseRoundedBars.setVisible(supportsRounding);
+        }
+        if (mPulseColor != null) {
+            mPulseColor.setVisible(supportsColoring);
+        }
     }
 
     @Override
