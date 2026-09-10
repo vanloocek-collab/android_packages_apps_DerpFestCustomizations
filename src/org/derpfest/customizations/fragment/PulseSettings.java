f1694281008de794094617ef728c8ed72d419319
NurKeinNeid <nurkeinneid@derpfest.org>
2026-09-10 19:28:30 +0200

    DerpFestCustomizations: Hide unused Pulse bar count and haptics prefs
    Particle does not draw bars, and bass haptics needs a vibrator.
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/derpfest/customizations/fragment/PulseSettings.java
+++ src/org/derpfest/customizations/fragment/PulseSettings.java
@@ -14,16 +14,22 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import org.derpfest.customizations.utils.DeviceUtils;
+
 public class PulseSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_PULSE_RENDERER = "pulse_renderer";
     private static final String KEY_PULSE_ROUNDED_BARS = "pulse_rounded_bars";
     private static final String KEY_PULSE_COLOR = "pulse_color";
+    private static final String KEY_PULSE_BAR_COUNT = "pulse_bar_count";
+    private static final String KEY_PULSE_HAPTICS = "pulse_haptics_enabled";
 
     private ListPreference mPulseRenderer;
     private Preference mPulseRoundedBars;
     private Preference mPulseColor;
+    private Preference mPulseBarCount;
+    private Preference mPulseHaptics;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         mPulseRenderer = findPreference(KEY_PULSE_RENDERER);
         mPulseRoundedBars = findPreference(KEY_PULSE_ROUNDED_BARS);
         mPulseColor = findPreference(KEY_PULSE_COLOR);
+        mPulseBarCount = findPreference(KEY_PULSE_BAR_COUNT);
+        mPulseHaptics = findPreference(KEY_PULSE_HAPTICS);
+
+        if (mPulseHaptics != null && !DeviceUtils.hasVibrator(getContext())) {
+            mPulseHaptics.setVisible(false);
+        }
 
         if (mPulseRenderer != null) {
             mPulseRenderer.setOnPreferenceChangeListener(this);
@@ -52,6 +64,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     /**
      * Rounding is only applied by SolidLine and Waveform in SystemUI.
      * Retro VU uses hardcoded segment colors and ignores pulse_color.
+     * Particle does not draw bars, so bar count has no visible effect.
      */
     private void updatePreferenceVisibility(String rendererValue) {
         if (rendererValue == null) {
@@ -61,12 +74,16 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         boolean supportsRounding = "solid".equals(rendererValue)
                 || "waveform".equals(rendererValue);
         boolean supportsColoring = !"retro".equals(rendererValue);
+        boolean supportsBarCount = !"particle".equals(rendererValue);
 
         if (mPulseRoundedBars != null) {
             mPulseRoundedBars.setVisible(supportsRounding);
         }
         if (mPulseColor != null) {
             mPulseColor.setVisible(supportsColoring);
+        }
+        if (mPulseBarCount != null) {
+            mPulseBarCount.setVisible(supportsBarCount);
         }
     }
 
